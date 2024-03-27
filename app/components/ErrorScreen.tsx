20365753fcca5cc6759af5b02da1f97e73120529
Paulo Ricardo <paulorsscontato@hotmail.com>
2024-03-26 23:32:10 -0300

    feat: update error screen component

--- app/components/ErrorScreen.tsx
+++ app/components/ErrorScreen.tsx
@@ -1,14 +1,4 @@
-import { useFetcher } from '@remix-run/react';
-
-const pageSize = '200';
-
 export const ErrorScreen = () => {
-  const fetcher = useFetcher();
-
-  const onTryAgain = () => {
-    fetcher.load(`?index&pageSize=${pageSize}`);
-  };
-
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <div className="max-w-md p-8 bg-white border rounded shadow-lg">
@@ -16,12 +6,6 @@ export const ErrorScreen = () => {
         <p className="text-gray-700 text-lg mb-6">
           Something went wrong when trying to load the pokemons.
         </p>
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-          onClick={onTryAgain}
-        >
-          Try again
-        </button>
       </div>
     </div>
   );
